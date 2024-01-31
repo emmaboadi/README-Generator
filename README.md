3e08ad5838890f119e57da0c1320c6dabb46d0c4
Jennifer Parsons <admin@Jennifer.local>
2024-01-31 11:06:24 +0000

    setting up inquirer prompt questions

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# README-Generator
+
+  # Flick Finder
+
+  ## Description
+   The aim of this project is to help users find recommendation of movies based  on movies  they've previously watched.
+
+  ## Table of Contents
+   Installation, Usage, Credits, License, Questions.
+
+  ## Installation
+  The App install several API that is used to design the weather dashboard. The Openweather APi was mainly used to fetch current and forecasted weather data
+
+  ## Usage
+  A user searches up a city and the weather dashboard displays current and forecasted weather conditions of that particular city.
+
+  ## License
+  MIT License
+
+  ## Contributing
+  undefined
+
+  ## Tests
+  dshfjkhskdjfh
+
+  ## Questions
+  undefined
+  
+  
+  - For additional question, Kindly contact me at Installation, Usage, Credits, License, Questions.
+
+
+  ## Installation
+  Installation, Usage, Credits, License, Questions.
+  
+
+    
--- index.js
+++ index.js
@@ -1,21 +1,98 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// array of questions for user
-const questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What is the process of installing your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How does a user uses your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license did you use for your project?',
+      choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'Boost Software License 1.0', 'Mozilla Public License 2.0'],
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'What are your contributions to the project?',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'How is your project tested?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your github username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email addtess for users to reach out',
+    },
+  ]);
+};
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const generateREADME = (answers) =>
+  `
+  # ${answers.title}
 
-// function to initialize program
-function init() {
+  ## Description
+   ${answers.description}
 
-}
+  ## Table of Contents
+   - [Installation](#installation)
+   - [Usage] (#usage)
+   - [License] (#license)
+   - [Contributing] (#contributing)
+   - [Tests] (#tests)
+   - [Questions] (#questions)
 
-// function call to initialize program
-init();
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  ![License](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+  ${answers.license}
+
+  ## Contributing
+  ${answers.contributing}
+
+  ## Tests
+  ${answers.test}
+
+  ## Questions
+  - For additional question, Kindly contact me at ${answers.email}.
+  GitHub: [${answers.github}](https://github.com/${answers.github})
+    `;
+
+    promptUser()
+    .then((answers) => writeFileAsync('sampleREADME.md', generateREADME(answers)))
+    .then(() => console.log('Successfully wrote to sampleREADME.md'))
+    .catch((err) => console.error(err));
+  
